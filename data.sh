d919548f12ed5f8e425851e1722147b975753bd7
Théo Turlin <tturlin@mac2110.irap.omp.eu>
2020-02-11 10:28:50 -0800

    Edit planck URL and add HI4PI URL

--- data.sh
+++ data.sh
@@ -7,4 +7,12 @@ cd data
 mkdir -p planck
 cd planck
 
-wget https://pla.esac.esa.int/pla-sl/data-action?MAP.MAP_OID=9300 -O ThermalDustModel_2048_R1.fits --timeout=0
+wget -O ThermalDustModel_2048_R1.20.fits "http://pla.esac.esa.int/pla/aio/product-action?MAP.MAP_ID=HFI_CompMap_ThermalDustModel_2048_R1.20.fits"
+
+cd ..
+
+# HI4PI data
+mkdir -p hi4pi
+cd hi4pi
+
+wget -O NHI_HPX.fits "https://lambda.gsfc.nasa.gov/product/foreground/fg_hi4pi_get.cfm"
